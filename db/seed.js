63ba9148b6069bfdb058708c1da4ac7bb3f703fb
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-18 17:11:43 -0600

    creating table

--- db/seed.js
+++ db/seed.js
@@ -28,16 +28,49 @@ const { client } = require('./index');
         }
     }
 
+    // Method: createTables
+    async function createTables() {
+        try {
+            console.log('Starting to build tables...');
+            await client.query(`
+
+            CREATE TABLE bentley(
+                "carId" SERIAL PRIMARY KEY,
+                "entreesName" VARCHAR(255) UNIQUE NOT NULL,
+                instructions VARCHAR(1000) UNIQUE NOT NULL,
+                "departmentId" INTEGER REFERENCES department("departmentId"),
+                description TEXT NOT NULL,
+                ingredients TEXT NOT NULL,
+                serving_size INTEGER NOT NULL,
+                time_to_prepare VARCHAR(255) UNIQUE NOT NULL,
+                imageOne VARCHAR(500) UNIQUE,
+                imageTwo VARCHAR(500) UNIQUE,
+                imageThree VARCHAR(500) UNIQUE,
+                imageFour VARCHAR(500) UNIQUE,
+                imageFIve VARCHAR(500) UNIQUE,
+                imageSix VARCHAR(500) UNIQUE,
+                imageSeven VARCHAR(500) UNIQUE,
+                imageEight VARCHAR(500) UNIQUE
+            );`);   
+    
+            console.log('Finished building tables.');
+            } catch (error) {
+            console.error('Error building tables!');
+            console.log(error);
+            }
+        }
+
     // Rebuild DB
     async function rebuildDB() {
         try {
-        client.connect();
-        console.log("Running DB function...")
-        await dropTables();
+            client.connect();
+            console.log("Running DB function...")
+            await dropTables();
+            await createTables();
 
         } catch (error) {
-        console.log("Error during rebuildDB!")
-        console.log(error.detail);
+            console.log("Error during rebuildDB!")
+            console.log(error.detail);
         }
     }
 
